f543fccbf83af9909d3585aac6f34d011537885e
Ananiassapalodeveloper <ananiassapalot@gmail.com>
2025-04-07 21:28:15 +0100

    tired

--- src/app/api/(reunioes)/ano/[id]/route.ts
+++ src/app/api/(reunioes)/ano/[id]/route.ts
@@ -2,44 +2,44 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
-// Buscar estados por ID
+// Buscar anos por ID
 export async function GET(req: Request, { params }: { params: { id: string } }) {
   const { id } = params;
-  const estados = await prisma.estado.findUnique({
+  const anos = await prisma.ano.findUnique({
     where: { id }
-    // include: { estado: true, carreira: true, servicos: { include: { servico: true, posicao: true } } }
+    // include: { ano: true, carreira: true, servicos: { include: { servico: true, posicao: true } } }
   });
 
-  if (!estados) return NextResponse.json({ error: "estados não encontrado" }, { status: 404 });
+  if (!anos) return NextResponse.json({ error: "anos não encontrado" }, { status: 404 });
 
-  return NextResponse.json(estados);
+  return NextResponse.json(anos);
 }
 
-// Atualizar estados
+// Atualizar anos
 export async function PUT(req: Request, { params }: { params: { id: string } }) {
   try {
     const { id } = params;
     const body = await req.json();
 
-    const estados = await prisma.estado.update({ where: { id }, data: body });
+    const anos = await prisma.ano.update({ where: { id }, data: body });
 
-    return NextResponse.json({ message: "estados atualizado com sucesso!", estados });
+    return NextResponse.json({ message: "anos atualizado com sucesso!", anos });
   } catch (error) {
-    return NextResponse.json({ error: "Erro ao atualizar estados" }, { status: 500 });
+    return NextResponse.json({ error: "Erro ao atualizar anos" }, { status: 500 });
   }
 }
 
-// Excluir estados (eliminação em cascata)
+// Excluir anos (eliminação em cascata)
 export async function DELETE(req: Request, { params }: { params: { id: string } }) {
   try {
     const { id } = params;
     
 
-    // Exclui o estados
-    await prisma.estado.delete({ where: { id } });
+    // Exclui o anos
+    await prisma.ano.delete({ where: { id } });
 
-    return NextResponse.json({ message: "estados excluído com sucesso!" });
+    return NextResponse.json({ message: "anos excluído com sucesso!" });
   } catch (error) {
-    return NextResponse.json({ error: "Erro ao excluir estados" }, { status: 500 });
+    return NextResponse.json({ error: "Erro ao excluir anos" }, { status: 500 });
   }
 }
--- src/app/api/(reunioes)/mes/[id]/route.ts
+++ src/app/api/(reunioes)/mes/[id]/route.ts
@@ -2,44 +2,44 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
-// Buscar estados por ID
+// Buscar meses por ID
 export async function GET(req: Request, { params }: { params: { id: string } }) {
   const { id } = params;
-  const estados = await prisma.estado.findUnique({
+  const meses = await prisma.meses.findUnique({
     where: { id }
-    // include: { estado: true, carreira: true, servicos: { include: { servico: true, posicao: true } } }
+    // include: { meses: true, carreira: true, servicos: { include: { servico: true, posicao: true } } }
   });
 
-  if (!estados) return NextResponse.json({ error: "estados não encontrado" }, { status: 404 });
+  if (!meses) return NextResponse.json({ error: "meses não encontrado" }, { status: 404 });
 
-  return NextResponse.json(estados);
+  return NextResponse.json(meses);
 }
 
-// Atualizar estados
+// Atualizar meses
 export async function PUT(req: Request, { params }: { params: { id: string } }) {
   try {
     const { id } = params;
     const body = await req.json();
 
-    const estados = await prisma.estado.update({ where: { id }, data: body });
+    const meses = await prisma.meses.update({ where: { id }, data: body });
 
-    return NextResponse.json({ message: "estados atualizado com sucesso!", estados });
+    return NextResponse.json({ message: "meses atualizado com sucesso!", meses });
   } catch (error) {
-    return NextResponse.json({ error: "Erro ao atualizar estados" }, { status: 500 });
+    return NextResponse.json({ error: "Erro ao atualizar meses" }, { status: 500 });
   }
 }
 
-// Excluir estados (eliminação em cascata)
+// Excluir meses (eliminação em cascata)
 export async function DELETE(req: Request, { params }: { params: { id: string } }) {
   try {
     const { id } = params;
     
 
-    // Exclui o estados
-    await prisma.estado.delete({ where: { id } });
+    // Exclui o meses
+    await prisma.meses.delete({ where: { id } });
 
-    return NextResponse.json({ message: "estados excluído com sucesso!" });
+    return NextResponse.json({ message: "meses excluído com sucesso!" });
   } catch (error) {
-    return NextResponse.json({ error: "Erro ao excluir estados" }, { status: 500 });
+    return NextResponse.json({ error: "Erro ao excluir meses" }, { status: 500 });
   }
 }
